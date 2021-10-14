e3a74fb03ccc823bc8174fd1b58cac318bd3c1b8
xc46 <52637873+xc46@users.noreply.github.com>
2021-10-14 12:17:32 -0400

    Libraries for openGL

---
+++ openglDependencies/cmake/assimp-5.0.1/build/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Drago/source/cpplibs/openglDependencies/cmake/assimp-5.0.1/contrib/zlib
+# Build directory: C:/Users/Drago/source/cpplibs/openglDependencies/cmake/assimp-5.0.1/build/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
